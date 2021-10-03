fc0e2f0a5cd9bfaf891a76de4e2688f11cf96386
1101321 <90212961+1101321@users.noreply.github.com>
2021-10-03 10:41:12 +0000

    [Start] 2-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -13,17 +13,23 @@ class TodolistsController < ApplicationController
     redirect_to todolist_path(list.id)
 
   end
-  
+
   def index
     @lists = List.all
-  
+
   end
-  
+
   def show
     @list = List.find(params[:id])
-    
+
   end
-  
+
+
+  def edit
+    @list = List.find(params[:id])
+
+  end
+
   private
   # ストロングパラメータ
   def list_params
